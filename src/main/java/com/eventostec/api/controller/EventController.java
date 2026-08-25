de204aa1d8115ca25cdc9567bbb764dc58a14c24
luanasantos-18 <luanacampos.santos18@gmail.com>
2026-08-25 16:02:37 -0300

    feat: II

---
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.eventostec.api.controller;
+
+import com.eventostec.api.domain.eventos.Event;
+import com.eventostec.api.domain.eventos.EventRequestDTO;
+import com.eventostec.api.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+
+    @Autowired
+    private EventService eventservice;
+
+    public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
+        Event newEvent = this.eventservice.createEvent(body);
+        return ResponseEntity.ok(newEvent);
+    }
+}
