b66472a9043f39ff0d6589f8be9b2a9b3a9efb17
luanasantos-18 <luanacampos.santos18@gmail.com>
2026-08-25 17:36:08 -0300

    feat: fix para funcionar a api

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -5,6 +5,7 @@ import com.eventostec.api.domain.eventos.EventRequestDTO;
 import com.eventostec.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class EventController {
     @Autowired
     private EventService eventservice;
 
+    @PostMapping
     public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
         Event newEvent = this.eventservice.createEvent(body);
         return ResponseEntity.ok(newEvent);
--- src/main/java/com/eventostec/api/service/EventService.java
+++ src/main/java/com/eventostec/api/service/EventService.java
@@ -3,6 +3,7 @@ package com.eventostec.api.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.eventos.Event;
 import com.eventostec.api.domain.eventos.EventRequestDTO;
+import com.eventostec.api.domain.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,10 @@ public class EventService {
 
     @Autowired
     private AmazonS3 s3Client;
+    private EventRepository eventRepository;
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
 
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
@@ -37,8 +42,10 @@ public class EventService {
         newEvent.setDescription(data.description());
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDate(new Date(data.date()));
-        newEvent.setImgUrl(imgUrl);
+        newEvent.setImgUrl("imgteste");
+        newEvent.setRemote(data.remote());
 
+        eventRepository.save(newEvent);
         return newEvent;
     }
 
